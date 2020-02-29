a098a199bdce957939fc5aa376c3ac2ce65d17c1
Daniel <daniel.p.masters@gmail.com>
2020-02-29 08:40:11 -0600

    working on improving flow of application

--- budgetr.js
+++ budgetr.js
@@ -1,3 +1,7 @@
+function runProgram() {
+
+
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -18,7 +22,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
+    // run the initPrompt function after the connection is made to prompt the user
 
 });
 readCatagories();
@@ -30,155 +34,163 @@ function readCatagories() {
     connection.query("SELECT * FROM cats", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++)
-            console.log("ID: " + res[i].id + " || Name: " + res[i].cat_name + " || Amount: " + res[i].amount +"$");
+            console.log("ID: " + res[i].id + " || Name: " + res[i].cat_name + " || Amount: " + res[i].amount + "$");
 
-        start();
+        initPrompt();
 
     });
 }
 
-function start() {
-    inquirer
-        .prompt({
-            name: "initPrompt",
-            type: "list",
-            message: "Would you like to [ADD] a catagory, or [MOD] money in an existing catagory?",
-            choices: ["ADD", "MOD"]
-        })
-        .then(function (answer) {
-            switch (answer.initPrompt) {
-                case "ADD":
-                    modCat()
-                    break;
-                case "MOD":
-                    updateMoney();
-                    break;
-                default:
-                    help()
-            }
-        })
-}
 
 
-function updateMoney() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is the ID of the catagory you'd like to update?",
-                name: "cat_id"
-            },
-            {
+
+    function initPrompt() {
+        inquirer
+            .prompt({
+                name: "initPrompt",
                 type: "list",
-                message: "Would you like to add or remove money?",
-                choices: ["ADD", "REMOVE"],
-                name: "add_or"
-            }
+                message: "Would you like to [ADD] a catagory, or [MOD] money in an existing catagory?",
+                choices: ["ADD", "MOD"]
+            })
+            .then(function (answer) {
+                switch (answer.initPrompt) {
+                    case "ADD":
+                        modCat()
+                        break;
+                    case "MOD":
+                        updateMoney();
+                        break;
+                    default:
+                        help()
+                }
+            })
+    }
 
-        ]).then(answer => {
-            var id = parseInt(answer.cat_id)
-            if (answer.add_or === "ADD") {
-                addMoney(cat_id);
-            } else if (answer.add_or === "REMOVE") {
-                removeMoney(cat_id)
-            }
-        })
-}
 
-function modCat() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is the name of the catagory you'd like to add?",
-                name: "cat_name"
-            }
-        ]).then(answer => {
-            var query = connection.query(
-                "INSERT INTO cats SET ?",
+    function updateMoney() {
+        inquirer
+            .prompt([
                 {
-                    cat_name: answer.cat_name
+                    type: "input",
+                    message: "What is the ID of the catagory you'd like to update?",
+                    name: "cat_id"
                 },
-                function (err, res) {
+                {
+                    type: "list",
+                    message: "Would you like to add or remove money?",
+                    choices: ["ADD", "REMOVE"],
+                    name: "add_or"
+                }
+
+            ]).then(answer => {
+                var id = parseInt(answer.cat_id)
+                if (answer.add_or === "ADD") {
+                    addMoney(id);
+                } else if (answer.add_or === "REMOVE") {
+                    removeMoney(id)
+                }
+            })
+    }
+
+    function modCat() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the name of the catagory you'd like to add?",
+                    name: "cat_name"
+                }
+            ]).then(answer => {
+                var query = connection.query(
+                    "INSERT INTO cats SET ?",
+                    {
+                        cat_name: answer.cat_name
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log(res.affectedRows + "catagory added!")
+                    })
+                console.log(query.sql);
+                initPrompt();
+            })
+
+    }
+
+    function addMoney(id) {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "How much would you like to add?",
+                    name: "add_amount"
+                }
+            ]).then(answer => {
+                var query = "SELECT * FROM cats WHERE id=" + id
+                connection.query(query, function (err, res) {
                     if (err) throw err;
-                    console.log(res.affectedRows + "catagory added!")
+                    console.log("Updating...")
+
+                    var addedAmount = parseInt(answer.add_amount)
+                    var query = connection.query(
+                        "UPDATE cats SET ? WHERE ?",
+                        [
+                            {
+                                amount: res[0].amount + addedAmount
+                            },
+                            {
+                                id: id
+                            }
+                        ],
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log(res.affectedRows + " products updated!\n");
+                        }
+                    );
+
+                    // logs the actual query being run
+                    console.log(query.sql);
                 })
-            console.log(query.sql);
-            start();
-        })
-
-}
-
-function addMoney(cat_id) {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "How much would you like to add?",
-                name: "add_amount"
-            }
-        ]).then(answer => {
-            var query = "SELECT * FROM cats WHERE id=" + cat_id
-            connection.query(query, function (err, res) {
-                if (err) throw err;
-                console.log("Updating...")
-
-                var addedAmount = parseInt(answer.add_amount)
-                var query = connection.query(
-                    "UPDATE cats SET ? WHERE ?",
-                    [
-                        {
-                            amount: res[0].amount + addedAmount
-                        },
-                        {
-                            id: id
-                        }
-                    ],
-                    function (err, res) {
-                        if (err) throw err;
-                        console.log(res.affectedRows + " products updated!\n");
-                    }
-                );
-
-                // logs the actual query being run
-                console.log(query.sql);
             })
-        })
-}
+    }
 
-function removeMoney(cat_id){
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "How much would you like to remove?",
-                name: "remove_amount"
-            }
-        ]).then(answer => {
-            var query = "SELECT * FROM cats WHERE id=" + cat_id
-            connection.query(query, function (err, res) {
-                if (err) throw err;
-                console.log("Updating...")
+    function removeMoney(id) {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "How much would you like to remove?",
+                    name: "remove_amount"
+                }
+            ]).then(answer => {
+                var query = "SELECT * FROM cats WHERE id=" + id
+                connection.query(query, function (err, res) {
+                    if (err) throw err;
+                    console.log("Updating...")
 
-                var removedAmount = parseInt(answer.remove_amount)
-                var query = connection.query(
-                    "UPDATE cats SET ? WHERE ?",
-                    [
-                        {
-                            amount: res[0].amount - removedAmount
-                        },
-                        {
-                            id: id
+                    var removedAmount = parseInt(answer.remove_amount)
+                    var query = connection.query(
+                        "UPDATE cats SET ? WHERE ?",
+                        [
+                            {
+                                amount: res[0].amount - removedAmount
+                            },
+                            {
+                                id: id
+                            }
+                        ],
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log(res.affectedRows + " products updated!\n");
                         }
-                    ],
-                    function (err, res) {
-                        if (err) throw err;
-                        console.log(res.affectedRows + " products updated!\n");
-                    }
-                );
+                    );
 
-                // logs the actual query being run
-                console.log(query.sql);
+                    // logs the actual query being run
+                    console.log(query.sql);
+                })
             })
-        })
+
+    }
+
 }
+
+runProgram();
